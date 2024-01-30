dda0dee0f3f4b84e315c0aa095ef6c51a320afd2
yutaasakura96 <yuta.asakura.se@gmail.com>
2024-01-30 22:04:28 +0900

    Starting conditions
    -Creating the starting conditions of the web application.
    -Selecting elements using DOM manipulation.

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+// SELECTING ELEMENTS
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+
+// STARTING CONDITIONS
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');

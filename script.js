8a90da3527d48488bc0a0c0f22e5178857e250ef
yutaasakura96 <yuta.asakura.se@gmail.com>
2024-02-01 20:19:26 +0900

    "NEW GAME" button functionality
    -Added a game functionality where in when the player clicks the 'NEW GAME' button, the game resets to it's initial condition including the scores and the background display.

--- script.js
+++ script.js
@@ -13,11 +13,6 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
-
 // PLAYER SWITCH FUNCTION
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -26,10 +21,30 @@ const switchPlayer = function () {
   player0El.classList.toggle('player--active');
   player1El.classList.toggle('player--active');
 };
+
+let scores, currentScore, activePlayer, playing;
 // STARTING CONDITIONS
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+const initialization = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  // 1. set the current score and total score to 0
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  // 2.remove the dice
+  diceEl.classList.add('hidden');
+  // 3.remove winner class
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  // 4.Switch to player 0
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+initialization();
 
 // ROLLING DICE FUNCTIONALITY
 btnRoll.addEventListener('click', function () {
@@ -79,3 +94,5 @@ btnHold.addEventListener('click', function () {
     }
   }
 });
+
+btnNew.addEventListener('click', initialization);
